db5ff0e2ae7eda6ac8a159f402e646f4692b38b8
Bryan Hickey <bh@bigmanmedia.com.au>
2021-08-17 14:22:23 +1000

    added netlfiy build hook plugin

---
+++ dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Website',
+            apiId: '80dc43a4-6f4b-495c-804a-1c8de096226c',
+            buildHookId: '611b30daa290cfb8f82b85b3',
+            name: 'vigilant-bassi-cd9e4b'
+          }
+        ]
+      }
+    }
+  ]
+}
